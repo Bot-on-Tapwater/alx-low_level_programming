c92fe30d646653da35c4f645ddee4ae5eedac424
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-02-16 16:30:36 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string using printf
+ *
+ * Description - Prints out the string
+ * "with proper grammar, but the outcome is
+ * a piece of art," using printf
+ * Return: Returns zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
