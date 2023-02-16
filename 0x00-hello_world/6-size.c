5adf4411fbfc38fc1d3c8468f0b2baf1aec04a4a
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-02-16 19:39:15 +0300

    UNIX is basically ...

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print various types
  *
+ * Description - uses printf() and sizeof() to print the sizes
+ * of various types
  * Return: Returns zero
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu bytes(s)", (unsigned long)sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n",
+		      (unsigned long)sizeof(long long));
+	printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
