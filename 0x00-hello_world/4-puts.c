ff492320dc8387ab0beb7abf79ccc89f0e43c76b
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-02-16 16:01:53 +0300

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out string using puts() function
+ *
+ * Description: Prints out the string "\"Programming is like
+ * building a multilingual puzzle\n" using puts()
+ * Return: Returns zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
