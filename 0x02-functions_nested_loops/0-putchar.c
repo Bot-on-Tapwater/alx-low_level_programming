17ee298ca37eff580dbb5f19bdf65ee45e9b50e8
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-02-21 09:37:35 +0300

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * main - entry point
+ *
+ * Return: Returns zero
+ */
+int main(void)
+{
+	char char1 = '_';
+	char char2 = 'p';
+	char char3 = 'u';
+	char char4 = 't';
+	char char5 = 'c';
+	char char6 = 'h';
+	char char7 = 'a';
+	char char8 = 'r';
+
+	_putchar(char1);
+	_putchar(char2);
+	_putchar(char3);
+	_putchar(char4);
+	_putchar(char5);
+	_putchar(char6);
+	_putchar(char7);
+	_putchar(char8);
+	_putchar(10);
+	return (0);
+}
+
+
