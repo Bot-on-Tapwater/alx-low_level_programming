f7efd067b6448aa20cd9ab9eec6d6aa1da89c3c3
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-02-21 11:43:14 +0300

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * _abs - gives absolute value of an integer
+ * @i: integer
+ *
+ * Return: returns absolute value of i
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (i * -1);
+	}
+	else
+	{
+		return (i);
+	}
+}
