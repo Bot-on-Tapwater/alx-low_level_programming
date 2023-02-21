500954199a89a68ed622c96f8c2a0db502968791
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-02-21 20:29:07 +0300

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @i: integer
+ * @j: integer
+ *
+ * Return: returns sum of i & j
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
