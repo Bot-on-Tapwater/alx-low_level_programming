09423225671e50aa22ccc244053aa2036d99a7e7
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-02-21 19:55:30 +0300

    Task 9

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,5 @@ int print_last_digit(int i)
 {
 	if (i < 0)
 	{
-		i *= -1;
-		_putchar('0' + i % 10);
-		return (i % 10);
-	}
-	else
-	{
-		int a = -(i % 10);
-		_putchar(a + '0');
-		return (a + '0');
-	}
+		i 
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,55 @@
 #include "main.h"
+
+/**
+ * times_table - prints 9x9 timetable
+ *
+ * Return: returns void
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int prod;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			prod = i * j;
+			if (!(prod == 0 || prod < 10))
+			{
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				if ((i * (j + 1)) < 10)
+				{
+					_putchar(prod + '0');
+					if (j < 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+				else
+				{
+					_putchar(prod + '0');
+					if (j < 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
