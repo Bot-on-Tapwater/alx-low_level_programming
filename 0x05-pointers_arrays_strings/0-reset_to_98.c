989b2eabc4306f6648f03daa56189b45e7991316
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-02-27 15:56:54 +0300

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates value of an integer to 98
+ * @n: points to the address of a variable holding an integer
+ *
+ * Return: returns nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
