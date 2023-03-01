e966e66bc53b6a915e89b66e079221aa9b050f50
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-03-01 09:34:28 +0300

    strncpy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -47,4 +47,3 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	return (dest);
 }
-
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncpy - concatenates two strings
+ * @dest: the destination string
+ * @src: our source string
+ * @n: point of terminaton for src
+ *
+ * Return: returns the combined string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count = 0; /* Counts number of char in *dest */
+	int i; /* integer to be used in our for loop expressions */
+	int ind_src = 0; /* tracks which index we are on src starting from index 0 */
+
+	while (dest[count] != '\0')
+	{
+		count++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[ind_src];
+		ind_src++; /* move to subsequent index */
+	}
+	return (dest);
+}
