5d6f9d9474fdb30288c1f6a284a8d8f5fd76f709
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-03-06 20:04:16 +0300

    Chess is mental torture

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,39 +9,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int count = 0; /* looping through haystack */
-	int cnt_ndl = 0; /* looping through needle */
-	int full_ndl = 0;
-	int start_ind = -1;
-
-	if (haystack == NULL || needle == NULL)
-		return (0);
-
-	while (haystack[count] != '\0')
+	for (; *haystack != '\0'; haystack++)
 	{
-		while (needle[cnt_ndl] != '\0')
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
 		{
-			/* printf("\nCurrently at needle[%d]\n", cnt_ndl); */
-			if (haystack[count] == needle[cnt_ndl])
-			{
-				full_ndl++;
-				if (haystack[count + 1] == needle[cnt_ndl + 1])
-				{
-					if (start_ind < 0)
-						start_ind = count;
-				}
-				else
-				{
-					full_ndl = 0;
-					start_ind = -1;
-				}
-			}
-			cnt_ndl++;
-			if (cnt_ndl > 0 && cnt_ndl == full_ndl)
-				return (&(haystack[start_ind]));
+			l++;
+			p++;
 		}
-		count++;
-		cnt_ndl = 0;
+
+		if (*p == '\0')
+			return (haystack);
 	}
+
 	return (0);
 }
