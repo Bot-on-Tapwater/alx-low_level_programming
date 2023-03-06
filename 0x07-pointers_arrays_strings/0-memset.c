c79fa7c38c0beacbd36b4fcb344a1828eb19a07e
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-03-06 12:43:48 +0300

    strchr

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,7 +10,8 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int count = 0; /* variable to check if n bytes of memory have been filled */
+	unsigned int count = 0; /* variable to check if */
+				/* n bytes of memory have been filled */
 
 	while (count < n) /* loop terminates when n bytes of memory filled */
 	{
