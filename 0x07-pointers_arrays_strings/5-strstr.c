6e213cb5505b4cd3ed2f7276716ffb6a12541acf
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-03-08 16:10:39 +0300

    My primary goal of hacking was the...

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,9 +9,13 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; haystack++)
+	for (; *haystack != '\0'; haystack++) /* for loop to loop over xters in the string haystack 
+											we stop the loop once we get to the null terminator ('\0')
+											first expression missing since we don't need to initialize variable haystack
+											cause haystack is a parameter hence already initialized*/
 	{
-		char *l = haystack;
+		char *l = haystack; /* The pointer l points to the  base address of haystack, if base address of haystack moves by by one on the subsequent iteration of the for loop the */
+		printf("\nhaystack is base address of string haystack: %p, which is equal to address of first character i.e haystack[0]:%p\n", haystack, &(haystack[0]));
 		char *p = needle;
 
 		while (*l == *p && *p != '\0')
