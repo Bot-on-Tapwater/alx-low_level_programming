2e29f564dc0a452b3ad2370f807c33f44244b98f
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-03-10 10:52:47 +0300

    Silence is argument carried out...

---
+++ 0x09-static_libraries/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: always 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
