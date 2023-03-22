1ad13c9aaa1c8de77f70ec1c5d0548b7ce0f2e86
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-03-22 18:15:34 +0300

    A goal is not always meant...

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sums two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: sum of a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mulitiplies two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - divides two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
