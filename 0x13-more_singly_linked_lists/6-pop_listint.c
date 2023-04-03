9e242b4a757c44248c2c7660dcab5b2b0d5063b5
Bot_on_Tapwater <mundabrandon@outlook.com>
2023-04-03 15:45:24 +0300

    Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head node of a linked list
+ * @head: pointer to a pointer of head
+ *
+ * Return: NULl or head node's data(n)
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *new_head; /* pointer to second element */
+	int currentHead_n; /* value of n for current head */
+
+	new_head = (*head)->next; /* assign second element to new_head */
+	currentHead_n = (*head)->n;
+
+	free(*head); /* delete current head */
+
+	*head = new_head; /* assign head to second element */
+
+	return (currentHead_n);
+}
