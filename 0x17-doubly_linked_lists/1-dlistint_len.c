21cc01e339a69ec5b9c23a8249ea484ce40a8ac0
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-05-18 14:47:01 +0300

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,18 +7,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	const dlistint_t *current = h;
 	size_t elem_count = 0; /* no. of elems */
 
-	if (h == NULL) /* check if NULL */
-	{
-		/* return (NULL); */
-		exit(0);
-	}
-
-	while (h != NULL) /* increment counter */
+	while (current != NULL) /* increment counter */
 	{
 		elem_count++;
-		h = h->next;
+		current = current->next;
 	}
 
 	return (elem_count);
