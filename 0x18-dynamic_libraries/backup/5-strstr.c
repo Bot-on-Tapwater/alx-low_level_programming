e1e04a9f81a6fa647d86c0a27ed74744b143e4f6
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-05-22 10:17:35 +0300

    Without libraries what have we?

--- 0x18-dynamic_libraries/backup/5-strstr.c
+++ 0x18-dynamic_libraries/backup/5-strstr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strstr - finds occurence of substring in a string
  * @haystack: target string
@@ -9,13 +9,9 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; haystack++) /* for loop to loop over xters in the string haystack 
-											we stop the loop once we get to the null terminator ('\0')
-											first expression missing since we don't need to initialize variable haystack
-											cause haystack is a parameter hence already initialized*/
+	for (; *haystack != '\0'; haystack++)
 	{
-		char *l = haystack; /* The pointer l points to the  base address of haystack, if base address of haystack moves by by one on the subsequent iteration of the for loop the */
-		printf("\nhaystack is base address of string haystack: %p, which is equal to address of first character i.e haystack[0]:%p\n", haystack, &(haystack[0]));
+		char *l = haystack;
 		char *p = needle;
 
 		while (*l == *p && *p != '\0')
