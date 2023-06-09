e12b4635f1a166dde2f9672e11881053df9262b0
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-06-09 19:02:25 +0300

    Betty errors

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,18 +9,17 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	/* Get key_index using key_index() func */
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
 	/* Go to the Node/linked_list associated with Key index */
+	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 	{
 		return (NULL);
 	}
-
-	index = key_index((const unsigned char *)key, ht->size);
 
 	if (index >= ht->size)
 		return (NULL);
