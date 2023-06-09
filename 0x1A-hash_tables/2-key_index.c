d4eac9956715b7393c3af361c0a0e117464d2e7b
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-06-09 15:00:15 +0300

    key->index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the index of a key
+ * @key: string "key"
+ * @size: Size of the array of nodes
+ * Return: The array index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_code = hash_djb2(key);
+	unsigned long int index = hash_code % size;
+
+	return (index);
+}
